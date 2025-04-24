8d86162ddd4e4a89e19ce08c3ec645bb7d13e670
batyrkhan09 <toktarbekov.b0905@gmail.com>
2025-04-24 21:46:11 +0500

    fix: обновлён CORS для нового Netlify домена

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import os
 from time import time
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": ["https://voice-access.netlify.app"]}})
+
 init_db()
 
 # === 🔐 Ограничение попыток входа ===
